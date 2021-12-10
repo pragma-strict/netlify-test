db7c3045dc0a5c9515aa27606a40c573aa9e9a06
Ian <ianrdejong@gmail.com>
2021-12-09 23:16:50 -0800

    Test edit

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,9 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <p>
+          Here's an entirely new paragraph! Hello, beautiful world!
+        </p>
       </main>
 
       <Footer />
